b3f3c974ff0e59306562a2573c79e485a6ec37ea
Dan Solo <dan@nerdi.com>
2014-02-18 10:00:09 +1100

    add complete option and tests

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -47,7 +47,15 @@ class TodoItemsController < ApplicationController
       flash[:error] = "Todo item could not be deleted."
     end
     redirect_to todo_list_todo_items_path
-   end
+  end
+
+  def complete
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to todo_list_todo_items_path, notice: "Todo item marked as complete"
+
+  end
+
 
   private
     def find_todo_list
