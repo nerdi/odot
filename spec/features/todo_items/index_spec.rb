b6ad365e4d42814a11dd3d86242ff8ccf6b91aa9
Dan Solo <dan@nerdi.com>
2014-02-18 08:31:38 +1100

    add delete and tests to list item

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -2,13 +2,6 @@ require 'spec_helper'
 
 describe "Viewing todo items" do
 	let!(:todo_list) { TodoList.create(title: "Grocery list", description: "Groceries are plenty") }
-	
-  def visit_todo_list(list) 
-    visit "/todo_lists"
-    within "#todo_list_#{list.id}" do
-      click_link"List Items"
-    end
-  end
 
   it "displays the title of a todo list" do
     visit_todo_list(todo_list)
